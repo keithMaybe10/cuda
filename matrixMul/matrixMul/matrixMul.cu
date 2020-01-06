5bf8bf626c96b5d8de70efaf09b9f3548aa0203b
keithMaybe10 <keithmaybe10@gmail.com>
2020-01-06 21:51:20 +0800

    add shared memory, delete some debug file

--- matrixMul/matrixMul/matrixMul.cu
+++ matrixMul/matrixMul/matrixMul.cu
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory>
 #include <cuda_runtime.h>
 
 const short m = 3;
 const short n = 4;
 const short k = 2;
+
+const dim3 blockNum(1, 1, 1);
+const dim3 threadNum(8, 8, 1);
 
 template<typename T>
 void initalValue(T& a, const short length, const float value)
@@ -16,16 +20,20 @@ void initalValue(T& a, const short length, const float value)
 }
 
 template<typename T>
-void checkValue(T a, const short length)
+void checkValue(T a, const short length, const short col)
 {
 	for (short i = 0; i < length; i++)
 	{
-		printf("a[%d]: %.3f\n", i, a[i]);
+		printf("%.3f ", a[i]);
+		if ((i - 1) % col == 0)
+		{
+			printf("\n");
+		}
 	}
 	printf("\n");
 }
 
-__global__ void matrixMul(const float* a, const float* b, float*c, const short m, const short n, const short k)
+__global__ void matrixMul(const float* a, const float* b, float* c, const short m, const short n, const short k)
 {
 	const short col = threadIdx.x + blockIdx.x * blockDim.x;
 	const short row = threadIdx.y + blockIdx.y * blockDim.y;
@@ -39,6 +47,24 @@ __global__ void matrixMul(const float* a, const float* b, float*c, const short m
 		}
 		c[row * k + col] = c_temp;
 	}	
+}
+
+__global__ void matrixMulWithSharedMemory(const float* a, const float* b, float* c, const short m, const short n, const short k)
+{
+	const short col = threadIdx.x + blockIdx.x * blockDim.x;
+	const short row = threadIdx.y + blockIdx.y * blockDim.y;
+
+	__shared__ float VALUE[6];
+
+	if (row < m && col < k)
+	{
+		for (short i = 0; i < n; i++)
+		{
+			VALUE[row * k + col] += a[row * n + i] * b[i * k + col];
+		}
+		__syncthreads();
+	}
+	c[row * k + col] = VALUE[row * k + col];
 }
 
 int main()
@@ -63,8 +89,8 @@ int main()
 	initalValue(h_b, n * k, 2.0);	
 
 	// value check
-	checkValue(h_a, m * n);
-	checkValue(h_b, n * k);
+	checkValue(h_a, m * n, n);
+	checkValue(h_b, n * k, k);
 
 	// initial device memory
 	float* d_a = nullptr;		// m * n 
@@ -79,13 +105,21 @@ int main()
 	cudaMemcpy(d_a, h_a, m * n * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_b, h_b, n * k * sizeof(float), cudaMemcpyHostToDevice);
 
-	dim3 blockNum(1, 1, 1);
-	dim3 threadNum(4, 4, 1);
-
+	// calculate matrixs
 	matrixMul << <blockNum, threadNum >> > (d_a, d_b, d_c, m, n, k);
 
 	cudaMemcpy(h_c, d_c, m * k * sizeof(float), cudaMemcpyDeviceToHost);
-	checkValue(h_c, m * k);
+	checkValue(h_c, m * k, k);
+
+	// calculate matrix with shared memory
+	cudaMemset(d_c, 0, m * k * sizeof(float));
+	memset(h_c, 0, m * k * sizeof(float));
+
+	matrixMulWithSharedMemory << <blockNum, threadNum >> > (d_a, d_b, d_c, m, n, k);
+
+	// copy calculate result from device to host
+	cudaMemcpy(h_c, d_c, m * k * sizeof(float), cudaMemcpyDeviceToHost);
+	checkValue(h_c, m * k, k);
 
 	free(h_a);
 	free(h_b);
